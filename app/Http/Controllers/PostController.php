21c286a055a63493bec55cb7b93c26c17834783c
Muawia24 <amuawia666@gmail.com>
2025-10-05 15:10:38 +0300

    Implement update post functionality with validation and authorization checks

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,26 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function updatePost(Post $post, Request $request) {
+        if (auth()->user()->id != $post->user_id) {
+            return redirect('/');
+        }
+        $incomingData = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        $incomingData['title'] = strip_tags($incomingData['title']);
+        $incomingData['body'] = strip_tags($incomingData['body']);
+
+        $post->update($incomingData);
+        return redirect('/');
+    }
+
     public function showEditScreen(Post $post) {
         return view('edit-post', ['post' => $post]);
 
     }
+
     public function createPost(Request $request) {
         $incomingData = $request->validate([
             'title' => 'required',
